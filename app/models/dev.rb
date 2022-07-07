27ef0fa5f5c41df8703d308dd6d475d9f6ffcf1a
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-07 12:19:46 -0400

    deliverables met

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
-
+    has_many :freebies
+    has_many :companies, through: :freebies
+    def recieved_one? item_name
+        self.freebies.find_by(item_name: item_name).nil?? false: true
+    end
+    def give_away dev, freebie
+        if self.recieved_one? freebie.item_name
+            freebie.update( dev_id: dev.id)
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,13 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
+Freebie.create(item_name: 'sweats', value: 10, dev_id: 2, company_id: 2)
+Freebie.create(item_name: 'sweats', value: 10, dev_id: 3, company_id: 2)
+Freebie.create(item_name: 'sweats', value: 10, dev_id: 4, company_id: 2)
+Freebie.create(item_name: 'sweats', value: 10, dev_id: 1, company_id: 2)
+Freebie.create(item_name: 'shirt', value: 10, dev_id: 2, company_id: 1)
+Freebie.create(item_name: 'shirt', value: 10, dev_id: 3, company_id: 1)
+Freebie.create(item_name: 'shirt', value: 10, dev_id: 4, company_id: 1)
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
